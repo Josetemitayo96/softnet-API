cd998c51a7d0d6b41b437a4afdb5b124a37ddcff
josetemitayo96 <temitayojose@gmail.com>
2019-03-27 18:49:48 +0100

    fixed heroku issue

--- Router/router.js
+++ Router/router.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const mongoose = require('../config/config')
 
 
+router.get('/', (req, res)=>{
+    res.status(200).json({
+        message:'Hello softnet API'
+    })
+})
+
 router.post('/hello', msgDetails.getMsg);
 
 module.exports = router;
